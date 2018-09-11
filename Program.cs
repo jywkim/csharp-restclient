31e233b1ff298bb5579525210fb11d65e6acf1ae
jywkim <jkim589@gmail.com>
2018-09-10 23:13:28 -0400

    process json result

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 
 namespace WebAPIClient
@@ -16,15 +18,20 @@ namespace WebAPIClient
 
         private static async Task ProcessRepositories()
         {
+            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
+
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+
             var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
 
-            var msg = await stringTask;
-            Console.Write(msg);
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.name);
         }
     }
 }
